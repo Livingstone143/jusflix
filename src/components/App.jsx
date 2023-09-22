d5b8279d63b81a205681616919c07a33a28e9968
Livingstone143 <paramalla2004@gmail.com>
2023-09-23 04:54:33 +0530

    frist

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { CssBaseline } from '@mui/material';
+
+
+const App = () => {
+  return (
+    
+    <div>
+        <CssBaseline />App</div>
+  )
+}
+
+export default App
