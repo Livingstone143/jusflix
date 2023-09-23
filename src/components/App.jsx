9a07b43ed573f8eab8b3c6171d5f95a3696e57f6
Livingstone143 <paramalla2004@gmail.com>
2023-09-24 03:16:10 +0530

    styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
 import { CssBaseline } from '@mui/material';
-import {BrowserRouter , Routes ,Route} from 'react-router-dom';
-
-
+import { Routes, Route } from 'react-router-dom';
+import { NavBar, MovieInfo, Movies, Actors, Profile ,Not} from './ind';
+import useStyles from './styles'
 const App = () => {
-  return (<main>
-    <BrowserRouter>
+  const classes = useStyles();
+  return (<div className={classes.root}>
+    <CssBaseline />
+    <NavBar />
+    <main className={classes.content}>
+     <div className={classes.toolbar} />
+      
+      
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movie:id" element={<About />} />
-        <Route path="/contact:id" element={<Contact />} />
+
+        <Route path="/" element={<Movies />} />
+        <Route path="/Movie/:id" element={<MovieInfo />} />
+        <Route path="/actors/:id" element={<Actors />} />
+        <Route path="profile/:id" element={<Profile />} />
+        <Route path="*" element={<Not />} />
+        
+
       </Routes>
-    </BrowserRouter>
+
     </main>
+  </div>
   );
 };
 
---
+++ src/components/ind.js
@@ -0,0 +1,7 @@
+export {default as Movies} from './Movies/Movies';
+export {default as Actors} from './Actors/Actors';
+export {default as MovieInfo} from './MoviesInformation/MovieInfo';
+export {default as NavBar} from './NavBar/NavBar';
+export {default as Profile} from './Profile/Profile';
+export {default as Not} from './NOt/Not.jsx';
+ 
