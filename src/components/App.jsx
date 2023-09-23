37991748ac608e4ce03e22f1f8379e88dfbeb0ee
Livingstone143 <paramalla2004@gmail.com>
2023-09-23 15:11:14 +0530

    components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,19 @@
 import React from 'react'
 import { CssBaseline } from '@mui/material';
-import {Routes ,Route} from 'react-router-dom';
+import {BrowserRouter , Routes ,Route} from 'react-router-dom';
 
 
 const App = () => {
-  return (
-    
-    <div>
-        <CssBaseline />
-<main>
-  <Routes>
-    <Route exact path="/movie/:id">
-      movie info
-    </Route>
-    <Route exact path="/actor/:id">
-      Actors
-    </Route>  <Route exact path="/">
-      movies
-    </Route>  <Route exact path="/profile/:id">
-      [Profile]
-    </Route>
-  </Routes>
-</main>
-        </div>
-  )
-}
+  return (<main>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie:id" element={<About />} />
+        <Route path="/contact:id" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+    </main>
+  );
+};
 
-export default App
+export default App;
