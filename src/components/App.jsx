5ef18545d538854a1c04826e5c24dad647f9c5ef
Livingstone143 <paramalla2004@gmail.com>
2023-09-23 14:42:02 +0530

    sec

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import { CssBaseline } from '@mui/material';
+import {Routes ,Route} from 'react-router-dom';
 
 
 const App = () => {
   return (
     
     <div>
-        <CssBaseline />App</div>
+        <CssBaseline />
+<main>
+  <Routes>
+    <Route exact path="/movie/:id">
+      movie info
+    </Route>
+    <Route exact path="/actor/:id">
+      Actors
+    </Route>  <Route exact path="/">
+      movies
+    </Route>  <Route exact path="/profile/:id">
+      [Profile]
+    </Route>
+  </Routes>
+</main>
+        </div>
   )
 }
 
